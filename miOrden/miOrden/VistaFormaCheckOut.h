098d94263549b9efa1d8bc4fcff1d6a95a7e953f
Rodrigo Higuera <rhigueraa@gmail.com>
2011-07-15 19:23:33 -0500

    vista carrito added

---
+++ miOrden/miOrden/VistaFormaCheckOut.h
@@ -0,0 +1,16 @@
+//
+//  VistaFormaCheckOut.h
+//  miOrden
+//
+//  Created by Rodrigo Higuera on 7/15/11.
+//  Copyright 2011 ITAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SCTableViewModel.h"
+
+@interface VistaFormaCheckOut : UITableViewController <SCTableViewModelDelegate> {
+    SCTableViewModel *tableModel;
+}
+
+@end
