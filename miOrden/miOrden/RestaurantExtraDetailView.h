b90652f61e2dc7ac4ccad9036160ffe756d894d5
Zebas Barrios <zebasb@hotmail.com>
2011-07-16 18:07:45 -0500

    Added extra detail view for restaurants

---
+++ miOrden/miOrden/RestaurantExtraDetailView.h
@@ -0,0 +1,16 @@
+//
+//  RestaurantExtraDetailView.h
+//  miOrden
+//
+//  Created by Sebastian Barrios on 7/16/11.
+//  Copyright 2011 ITAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RestaurantExtraDetailView : UIViewController {
+    
+}
+
+@end
